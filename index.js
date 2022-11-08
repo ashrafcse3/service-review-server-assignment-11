e481d45ad0ef5f17be2969b0a5301941277aaf51
Ashraful Alam <ashrafcse3@gmail.com>
2022-11-08 22:15:52 +0000

    service fetched by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -30,6 +30,15 @@ async function run() {
             const result = await cursor.toArray();
 
             res.send(result);
+        })
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+
+            const service = await servicesCollection.findOne(query);
+
+            res.send(service);
         })
     }
     finally {
