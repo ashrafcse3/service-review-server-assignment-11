6daa9e8e4bba682b650f3f0349aa0204837a6ebb
Ashraful Alam <ashrafcse3@gmail.com>
2022-11-08 18:33:20 +0000

    basic server setup

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// A new file setup for the server side code because earlier commits were wrong
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+// using middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from eye specialist server');
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4qifqp4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri);
+
+app.listen(port, () => {
+    console.log(`Eye specialist app listening on port ${port} with nodemon`)
+})
